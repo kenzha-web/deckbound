8accdd82637b75f5e44779099dcc187e234b9ec5
kenzha-web <kenzha.web@gmail.com>
2025-07-07 19:51:29 +0500

    add game board

--- src/pages/home/board/GameBoard.tsx
+++ src/pages/home/board/GameBoard.tsx
@@ -1,3 +1,35 @@
+import { useGameStore } from '../../../store/game/game.store.ts';
+
 export function GameBoard() {
-    return <div>GameBoard</div>;
+    const { player, opponent } = useGameStore();
+
+    return (
+        <div>
+            <div>
+                <div>
+                    <h1>Opponent</h1>
+                    <p>HP: {opponent.health}</p>
+                    <p>Mana: {opponent.mana}</p>
+                </div>
+
+                <div>
+                    {opponent.deck
+                        .filter((card) => !card.isOnBoard)
+                        .slice(0, 5)
+                        .map((card) => (
+                            <button
+                                className="h-60 w-44 bg-yellow-300 shadow inline-block mx-1 rounded-lg"
+                                key={card.id}
+                            />
+                        ))}
+                </div>
+
+                <div>
+                    <h1>Player</h1>
+                    <p>HP: {player.health}</p>
+                    <p>Mana: {player.mana}</p>
+                </div>
+            </div>
+        </div>
+    );
 }
